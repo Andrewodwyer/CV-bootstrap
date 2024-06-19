11a4e4706fc397513033557ffbb2048140fbfdbe
Andrew O'Dwyer <andrewodwyer@yahoo.co.uk>
2024-06-19 10:26:49 +0000

    add error message 404

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,7 +1,7 @@
 function fetchGitHubInformation(event) {
 
     var username = $("#gh-username").val(); //use jquary to select the id and the value in that text field
-    if (!username) { // if the field is empty, no value
+    if (!username) { // if the field is empty, no value, this div will appear
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
         return;
     }
@@ -10,4 +10,22 @@ function fetchGitHubInformation(event) {
         `<div id="loader">
             <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
+// when then promise. .when takes a function as it first argument. in .when, we pass in a function that is getJSON
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`) //the value we optioaned from the user box
+    ).then(
+        function(response) { // the response is what came back from the .when, the getJSON function
+            var userData = response;
+            $("#gh-user-data").html(userInformationHTML(userData)); // response is stored in userData var and userData will be used as a argument in userInformationHTML function
+        },
+        function(errorResponse) {
+            if (errorResponse.status === 404) { //if the response has a 404 status, we'll desplay the the H2 in the html
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        });
 }
