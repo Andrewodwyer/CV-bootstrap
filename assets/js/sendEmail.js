243cfe642370e03612f32b402b819ea187019a58
Andrew O'Dwyer <andrewodwyer@yahoo.co.uk>
2024-06-19 15:11:20 +0000

    sendEmail.js changes to try and fix bug with from_email

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,5 +1,5 @@
 function sendMail(contactForm) {
-    emailjs.send("service_8bygsmp", "rosie", {
+    emailjs.send("gmail", "rosie", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "project_request": contactForm.projectsummary.value
